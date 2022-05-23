ed08d316e7e4738940ce00cc15e4589354410c89
yigtkaya <68725704+yigtkaya@users.noreply.github.com>
2022-05-23 21:43:34 +0300

    Add files via upload

---
+++ docs/docs.go
@@ -0,0 +1,19 @@
+// REST API with go-swagger, mongodb and openapi 2.0.
+//
+//
+//REST API with go-swagger, mongodb and openapi 2.0.
+//     Schemes: http, https
+//     Host: localhost:9090
+//     BasePath: /v1
+//     Version: 1.0.0
+//     License: MIT http://opensource.org/licenses/MIT
+//
+//     Consumes:
+//     - application/json
+//     - application/xml
+//
+//     Produces:
+//     - application/json
+//     - application/xml
+// swagger:meta
+package docs
